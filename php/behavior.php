3a5c013fef3c747814c0a6e4e804b30b256f5672
Aksh <your_akdesai@algoma.ca>
2024-06-28 23:42:59 -0400

    lab 7

---
+++ php/behavior.php
@@ -0,0 +1,12 @@
+<?php
+include 'C:/xampp/htdocs/termproject/includes/header.php';
+?>
+
+<div class="content">
+    <h1>Welcome to My Website</h1>
+    <p>This is a paragraph explaining something about your website.</p>
+</div>
+
+<?php
+include 'C:/xampp/htdocs/termproject/includes/footer.php';
+?>
